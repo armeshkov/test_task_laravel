1eaf5362783ee1975767c59ed99ab42023b38318
axmeshkov <axmeshkov@gmail.com>
2021-10-26 15:17:50 +0300

    reworked template

--- laravel-app/resources/views/companies/index.blade.php
+++ laravel-app/resources/views/companies/index.blade.php
@@ -8,11 +8,41 @@
     <title>Document</title>
 </head>
 <body>
-
-
-@foreach($companies as $company)
-    <p>{{ $company->id}}</p>
-@endforeach
-
+@extends('companies.base')
+@section('content')
+    <div class="container">
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">phone</th>
+            <th scope="col">website</th>
+            <th scope="col">logo</th>
+            <th scope="col">Created at</th>
+            <th scope="col">Updated at</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($companies as $company)
+            <tr>
+                <th scope="row">{{ $company->name }}</th>
+                <td>{{ $company->email }}</td>
+                <td>{{ $company->phone }}</td>
+                <td>{{ $company->website }}</td>
+                <td>{{ $company->logo }}</td>
+                <td>{{ $company->created_at }}</td>
+                <td>{{ $company->updated_at }}</td>
+                <td>
+                    <a class="btn btn-small btn-primary" href="{{ url('companies/' . $company->id) }}">Show</a>
+                    <a class="btn btn-small btn-success" href="">Edit</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    </div>
+@endsection
 </body>
 </html>
