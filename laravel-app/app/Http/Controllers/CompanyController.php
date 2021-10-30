b37604c3df0f76074e05f3f917c491aa5f42357c
axmeshkov <axmeshkov@gmail.com>
2021-10-30 09:19:35 +0300

    configured correct path to image

--- laravel-app/app/Http/Controllers/CompanyController.php
+++ laravel-app/app/Http/Controllers/CompanyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Company;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class CompanyController extends Controller
 {
@@ -30,14 +31,27 @@ class CompanyController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      */
     public function store(Request $request)
     {
 
+        $rules = array(
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => ['required', 'regex:/^((\+?7|8)(?!95[4-79]|99[08]|907|94[^0]|336)([348]\d|9[0-6789]|7[0247])\d{8}|\+?(99[^4568]\d{7,11}|994\d{9}|9955\d{8}|996[57]\d{8}|9989\d{8}|380[34569]\d{8}|375[234]\d{8}|372\d{7,8}|37[0-4]\d{8}))$/'],
+            'website' => 'url',
+            'logo' => 'image|dimensions:min_width=100,min_height=100'
+        );
+        $validator = Validator::make($request->post(), $rules);
+        if ($validator->fails()) {
+            return redirect(route('dashboardcompanies.create'))
+                ->withErrors($validator)
+                ->withInput();
+        }
 
         $folder = Auth::user()->id;
-        $logo = $request->file('logo')->store("images/$folder");
+        $logo = $request->file('logo')->store("companies/$folder");
 
         $company = Company::create([
             'name' => $request->name,
@@ -51,7 +65,6 @@ class CompanyController extends Controller
 
     /**
      * Display the specified resource.
-
      */
     public function show(Company $company)
     {
@@ -62,7 +75,7 @@ class CompanyController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -73,8 +86,8 @@ class CompanyController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -85,7 +98,7 @@ class CompanyController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
