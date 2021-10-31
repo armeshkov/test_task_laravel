0555a50104a510306a745238a781722117ba8f80
axmeshkov <axmeshkov@gmail.com>
2021-10-31 20:21:48 +0300

    added implementation of edit and update methods

--- laravel-app/app/Http/Controllers/CompanyController.php
+++ laravel-app/app/Http/Controllers/CompanyController.php
@@ -80,24 +80,46 @@ class CompanyController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param int $id
-     * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Company $company)
     {
-        //
+        $this->authorize('update', $company);
+        return view('companies.edit', ['company' => $company]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Company $company)
     {
-        //
+        $this->authorize('update', $company);
+        $rules = array(
+            'name' => 'required',
+            'email' => 'required|email',
+            //'phone' => ['required', 'regex:/^((\+?7|8)(?!95[4-79]|99[08]|907|94[^0]|336)([348]\d|9[0-6789]|7[0247])\d{8}|\+?(99[^4568]\d{7,11}|994\d{9}|9955\d{8}|996[57]\d{8}|9989\d{8}|380[34569]\d{8}|375[234]\d{8}|372\d{7,8}|37[0-4]\d{8}))$/'],
+            'phone' => 'required|min:10',
+            'website' => 'url',
+            'logo' => 'image|dimensions:min_width=100,min_height=100'
+        );
+        $validator = Validator::make($request->post(), $rules);
+        if ($validator->fails()) {
+            return redirect(route('dashboardcompanies.edit', ['company' => $company]))
+                ->withErrors($validator)
+                ->withInput();
+        }
+        $folder = Auth::user()->id;
+        $logo = $request->file('logo')->store("companies/$folder");
+        Company::where('id', $company->id)->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'website' => $request->website,
+            'logo' => $logo,
+            'user_id' => Auth::user()->id,
+        ]);
+        return Redirect::to(route('dashboardcompanies.show', $company))->with('message', 'Success!');
     }
 
     /**
