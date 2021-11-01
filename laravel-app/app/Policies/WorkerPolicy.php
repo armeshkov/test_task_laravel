63e4f3f445f72cd495762b9b462e1fa6b1073ddb
axmeshkov <axmeshkov@gmail.com>
2021-11-01 09:48:54 +0300

    created worker policy

---
+++ laravel-app/app/Policies/WorkerPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Company;
+use App\Models\User;
+use App\Models\Worker;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class WorkerPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     */
+    public function viewAny(User $user, Company $company)
+    {
+        dd('123');
+        return $user->id == $company->user_id;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Worker  $worker
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Worker $worker)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Worker  $worker
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Worker $worker)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Worker  $worker
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Worker $worker)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Worker  $worker
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Worker $worker)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Worker  $worker
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Worker $worker)
+    {
+        //
+    }
+}
