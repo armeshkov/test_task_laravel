5c6ad650d3cf766bb8357ef8a2ec1ae22a523719
axmeshkov <axmeshkov@gmail.com>
2021-11-02 10:30:40 +0300

    completed index template for workers

---
+++ laravel-app/resources/views/workers/index.blade.php
@@ -0,0 +1,43 @@
+@extends('companies.base')
+@section('content')
+    <div class="container mt-3 mb-3">
+        <h1>{{ $company->name }}</h1>
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Created at</th>
+                <th scope="col">Updated at</th>
+                <th scope="col">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($company->workers as $worker)
+                <tr>
+                    <th scope="row">{{ $worker->first_name }}</th>
+                    <td>{{ $worker->last_name }}</td>
+                    <td>{{ $worker->email }}</td>
+                    <td>{{ $worker->phone }}</td>
+                    <td>{{ $worker->created_at }}</td>
+                    <td>{{ $worker->updated_at }}</td>
+                    <td>
+                        <a class="btn btn-small btn-primary" href="{{ route('dashboardcompanies.show', ['company' => $company]) }}">Show</a>
+                        <a class="btn btn-small btn-success" href="{{ route('dashboardcompanies.edit', ['company' => $company]) }}">Edit</a>
+                        <form class="pt-2" action="{{ route('dashboardcompanies.destroy', ['company' => $company]) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="container text-center">
+        <a href="{{ route('dashboardcompanies.workers.create', ['company' => $company]) }}" class="btn btn-primary">Add worker</a>
+    </div>
+@endsection
