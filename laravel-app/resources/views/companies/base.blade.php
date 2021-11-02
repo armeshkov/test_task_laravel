f8aa63fe96dac421b5b8ecbe8413689d455aac82
axmeshkov <axmeshkov@gmail.com>
2021-11-02 11:44:07 +0300

    added route to dashboard

--- laravel-app/resources/views/companies/base.blade.php
+++ laravel-app/resources/views/companies/base.blade.php
@@ -14,7 +14,7 @@
     <div class="container px-0">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Dashboard</a>
+                <a class="navbar-brand" href="{{ route('dashboard') }}">Dashboard</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
