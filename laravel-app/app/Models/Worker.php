dd41a001562417c35336d78adf834a23d5a3488c
axmeshkov <axmeshkov@gmail.com>
2021-11-02 11:44:49 +0300

    added fillable fields

--- laravel-app/app/Models/Worker.php
+++ laravel-app/app/Models/Worker.php
@@ -9,6 +9,8 @@ class Worker extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['first_name', 'last_name', 'company_id','email', 'phone'];
+
     public function company() {
         return $this->belongsTo(Company::class);
     }
