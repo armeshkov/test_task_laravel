a53b5ed8d5e4a92f1fadcc5cd6032a8396437033
axmeshkov <axmeshkov@gmail.com>
2021-11-02 11:44:29 +0300

    completed templated of adding worker

--- laravel-app/resources/views/workers/create.blade.php
+++ laravel-app/resources/views/workers/create.blade.php
@@ -1,1 +1,37 @@
-123
+@extends('companies.base')
+@section('content')
+    <div class="container">
+        <h1>Add worker</h1>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form method="post" action="{{ route('dashboardcompanies.workers.store', ['company' => $company])}}">
+            @csrf
+            <div class="mb-3">
+                <label for="first_name" class="form-label">First Name</label>
+                <input type="text" class="form-control" id="first_name" name="first_name">
+            </div>
+            <div class="mb-3">
+                <label for="last_name" class="form-label">Last Name</label>
+                <input type="text" class="form-control" id="last_name" name="last_name">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email">
+            </div>
+            <div class="mb-3">
+                <label for="phone" class="form-label">Phone number</label>
+                <input type="text" class="form-control" id="phone" name="phone">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+@endsection
