84f4af20b11d03763f7d40472c66fa8e5f09cbbd
axmeshkov <axmeshkov@gmail.com>
2021-11-02 11:45:04 +0300

    implemented store method

--- laravel-app/app/Http/Controllers/WorkerController.php
+++ laravel-app/app/Http/Controllers/WorkerController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Company;
+use App\Models\Worker;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 
 class WorkerController extends Controller
 {
@@ -29,12 +33,31 @@ class WorkerController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Company $company)
     {
-        //
+        $rules = array(
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email',
+            //'phone' => ['required', 'regex:/^((\+?7|8)(?!95[4-79]|99[08]|907|94[^0]|336)([348]\d|9[0-6789]|7[0247])\d{8}|\+?(99[^4568]\d{7,11}|994\d{9}|9955\d{8}|996[57]\d{8}|9989\d{8}|380[34569]\d{8}|375[234]\d{8}|372\d{7,8}|37[0-4]\d{8}))$/'],
+            'phone' => 'required|min:10',
+        );
+        $validator = Validator::make($request->post(), $rules);
+        if ($validator->fails()) {
+            return redirect(route('dashboardcompanies.workers.create'))
+                ->withErrors($validator)
+                ->withInput();
+        }
+        $worker = Worker::create([
+           'first_name' => $request->first_name,
+           'last_name' => $request->last_name,
+           'company_id' => $company->id,
+           'email' => $request->email,
+           'phone' => $request->phone,
+        ]);
+        Session::flash('message', 'Successfully added worker!');
+        return Redirect::to(route('dashboardcompanies.workers.index', ['company' => $company]));
     }
 
     /**
